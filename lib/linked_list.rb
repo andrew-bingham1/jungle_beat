fbd93d5e26d9f842d3fd525519d033f047028202
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-04 14:09:05 -0700

    create count and to_string methods for LinkedList class, also cleans up append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -12,19 +12,32 @@ class LinkedList
   end
 
   def append(data)
-    
-    @nodes += 1
     @current_node = node
+    @nodes += 1
     node_string << "#{data} "
+
     if @head == nil
       @head = node
     end
+
     while current_node.next_node != nil 
       current_node = current_node.next_node
     end
+
     node = Node.new(data)
     
     return data
+
+  end
+
+  def count
+    @nodes
+  end
+
+  def to_string
+    node_string.chomp(" ")
+    node_string
+  
   end
 
 
