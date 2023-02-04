0760c867f5b4f7bb8472a7f7a8b50cfbc63fec50
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-04 13:58:26 -0700

    create initialize method for LinkedList class and define instance variables

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,7 +2,14 @@ require 'node.rb'
 
 class LinkedList
   
-  
+  attr_accessor :head, :node_string, :nodes, :current_node
+
+  def initialize
+    @current_node = current_node
+    @head = nil
+    @nodes = 0
+    @node_string = ""
+  end
 
 
 
