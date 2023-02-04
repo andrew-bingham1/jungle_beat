5c635feda3e0f0a9465d39d498935dd97094d73b
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-04 14:00:10 -0700

    create append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -11,6 +11,21 @@ class LinkedList
     @node_string = ""
   end
 
+  def append(data)
+    
+    @nodes += 1
+    @current_node = node
+    node_string << "#{data} "
+    if @head == nil
+      @head = node
+    end
+    while current_node.next_node != nil 
+      current_node = current_node.next_node
+    end
+    node = Node.new(data)
+    
+    return data
+  end
 
 
 end
