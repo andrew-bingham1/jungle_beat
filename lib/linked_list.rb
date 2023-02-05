1cd513e4c9208c3b038002954b594c837f6bd228
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-04 18:14:20 -0700

    create prepend method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,17 +2,21 @@ require 'node.rb'
 
 class LinkedList
   
-  attr_accessor :head, :node_string, :nodes, :current_node, :node
+  attr_accessor :head, :node_string, :nodes, :current_node, :node, :previous_node
 
   def initialize
-    @current_node = current_node
     @head = nil
     @nodes = 0
     @node_string = ""
-    # @node = node
+    @previous_node = previous_node
+    @current_node = current_node
+    
   end
 
   def append(data)
+    if current_node != nil
+      previous_node = current_node
+    end
     node = Node.new(data)
     @current_node = node
     @nodes += 1
@@ -40,5 +44,21 @@ class LinkedList
   
   end
 
+  def prepend(data)
+    node = Node.new(data)
+    previous_node = head
+    @nodes += 1
+    node_string[0,0] = "#{data} "
+    @head = node
+
+    if @nodes == 0
+      head.next_node = nil
+    else
+      head.next_node = previous_node
+    end
+
+    return data
+
+  end
 
 end
