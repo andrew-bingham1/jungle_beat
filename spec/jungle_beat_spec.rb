27c9f71a337a5a5b0a9aab7c27cfd772192299c0
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-06 14:40:20 -0700

    test: create initialize test for jungle_beat_spec

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -4,7 +4,10 @@ require './lib/linked_list'
 require './lib/jungle_beat'
 
 RSpec.describe JungleBeat do 
-  it 'xyz' do
+  it '#initialize' do
+    jb = JungleBeat.new
     
+    expect(jb).to be_a(JungleBeat)
+    expect(jb.list.head).to eq(nil)
   end
 end
