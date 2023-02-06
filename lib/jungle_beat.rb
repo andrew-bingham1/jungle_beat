228266bc8778b603acd267d131088116b7978e42
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-06 15:47:06 -0700

    fix: restores jungle_beat.rb

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,3 +1,21 @@
+require 'node.rb'
+require 'linked_list.rb'
+
+class JungleBeat
+  attr_accessor :list
+  def initialize
+    @list = LinkedList.new
+  end
+
+  def append(words)
+    words_string = words
+    words_arr = words_string.split(" ")
+    words_arr.each do |word|
+      list.append(word)
+    end
+
+    return self.list.node_string.chomp(" ")
+  end
 
   def count
     self.list.nodes
